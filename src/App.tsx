efe8fcc525e14e36ba72861a3337d75b0956ba7c
Matheus Balonecker <mathbalon@gmail.com>
2021-02-27 17:27:03 -0400

    splash screen android

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+import SplashScreen from 'react-native-splash-screen';
 import AppProvider from './hooks';
 import Routes from './routes';
+import { useAuth } from './hooks/auth';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#312e38" translucent />
-    <AppProvider>
-      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-        <Routes />
-      </View>
-    </AppProvider>
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  const { user } = useAuth();
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, [user]);
+
+  return (
+    <NavigationContainer>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#312e38"
+        translucent
+      />
+      <AppProvider>
+        <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+          <Routes />
+        </View>
+      </AppProvider>
+    </NavigationContainer>
+  );
+};
 
 export default App;
